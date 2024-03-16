f397a314c8cd1b9d022f5861d1410fdd6de79463
Grzegorz Wilkowiecki <wilkowiecki00@gmail.com>
2024-03-16 21:03:18 +0100

    pole prostokąta

--- exercise1.py
+++ exercise1.py
@@ -2,3 +2,4 @@ bok_a = 10.8
 
 pole_kw = bok_a * bok_a
 print(f'POLE KWADRATU : {pole_kw}')
+
---
+++ prostokat.py
@@ -0,0 +1,5 @@
+sciana_a = 5
+sciana_b = 5
+
+pole_pr = sciana_b * sciana_a
+print(f"Pole prostokąta = {pole_pr}")
